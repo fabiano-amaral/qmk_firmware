39b7272cabd3c0ce703577220c90da8af4df5953
PotHix <pothix@pothix.com>
2021-12-20 19:18:56 -0300

    Improving the layout so I can understand it better
    It also sets the manufaturer with my name ✨

--- keyboards/handwired/dactyl_promicro/config.h
+++ keyboards/handwired/dactyl_promicro/config.h
@@ -18,6 +18,26 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#include "config_common.h"
+#define PRODUCT         Dactyl cc
+
+/* key matrix size */
+// Rows are doubled-up
+#define MATRIX_ROWS 12
+#define MATRIX_COLS 6
+
+// wiring of each half
+#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
+
+#define DIODE_DIRECTION COL2ROW
+
+/* USB Device descriptor parameter */
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x3060
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    PotHix
+
 /* mouse config */
 #define MOUSEKEY_INTERVAL       20
 #define MOUSEKEY_DELAY          0
--- keyboards/handwired/dactyl_promicro/keymaps/pothix/keymap.c
+++ keyboards/handwired/dactyl_promicro/keymaps/pothix/keymap.c
@@ -8,25 +8,16 @@
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
 
-#define ONE_GRV LT(1,KC_GRAVE)
-#define CTL_Z LCTL_T(KC_Z)
-#define ALT_SHFT LALT(KC_LSFT)
-#define ALT_MENU LALT_T(KC_MENU)
-#define LG_QUOT LGUI_T(KC_QUOT)
-#define CTL_ESC LCTL_T(KC_ESC)
-#define CTL_SLS LCTL_T(KC_SLSH)
-#define LT2_COL LT(_RAISE, KC_SCLN)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_6x6(
-        KC_EQL , KC_1  ,KC_2   , KC_3  , KC_4  , KC_5  ,                                        KC_6   , KC_7  , KC_8  , KC_9  , KC_0  ,KC_BSPC,
-        KC_DEL , KC_Q  ,KC_W   , KC_E  , KC_R  , KC_T  ,                                        KC_Y   , KC_U  , KC_I  , KC_O  , KC_P  ,KC_MINS,
-        KC_BSPC, KC_A  ,KC_S   , KC_D  , KC_F  , KC_G  ,                                        KC_H   , KC_J  , KC_K  , KC_L  ,LT2_COL,LG_QUOT,
-        KC_LSFT, CTL_Z ,KC_X   , KC_C  , KC_V  , KC_B  ,                                        KC_N   , KC_M  ,KC_COMM, KC_DOT,CTL_SLS,KC_RSFT,
-        ONE_GRV,KC_QUOT,ALT_SHFT,KC_LEFT,KC_RGHT,KC_SPC ,                                       KC_ENT , KC_UP  ,KC_DOWN,KC_LBRC,KC_RBRC,TT(_LOWER),
-                                                KC_BSPC,ALT_MENU,KC_LGUI,       KC_RALT,CTL_ESC,KC_TAB ,
-                                                                KC_HOME,        KC_PGUP,
-                                                                KC_END ,        KC_PGDN
+        KC_EQL , KC_1  ,KC_2   , KC_3  , KC_4  , KC_5  ,                                        KC_6   , KC_7  , KC_8  , KC_9   , KC_0  ,KC_MINS,
+        KC_TAB , KC_Q  ,KC_W   , KC_E  , KC_R  , KC_T  ,                                        KC_Y   , KC_U  , KC_I  , KC_O   , KC_P  ,KC_BSLS,
+        KC_ESC,  KC_A  ,KC_S   , KC_D  , KC_F  , KC_G  ,                                        KC_H   , KC_J  , KC_K  , KC_L   ,KC_SCLN,KC_QUOT,
+        KC_LSFT, KC_Z  ,KC_X   , KC_C  , KC_V  , KC_B  ,                                        KC_N   , KC_M  ,KC_COMM, KC_DOT ,KC_SLSH,KC_RSFT,
+        KC_GRV,KC_BSLS,KC_LEFT ,KC_RGHT,KC_BSPC, KC_DEL,                                        KC_SPC ,KC_ENT , KC_UP , KC_DOWN,KC_LBRC,KC_RBRC,
+                                                KC_MINS,_______,KC_RBRC,        KC_RBRC,_______,KC_MINS ,
+                                                                KC_BSLS,        KC_BSLS,
+                                                                KC_LBRC,        KC_LBRC
     ),
     [_LOWER] = LAYOUT_6x6(
         _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                                       KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
