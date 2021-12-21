319c8a54a96ab7462f7ed2899198e0bff0c0e419
PotHix <pothix@pothix.com>
2021-12-21 14:30:52 -0300

    Add my dactyl-cc configuration

--- keyboards/handwired/dactyl_promicro/keymaps/pothix/keymap.c
+++ keyboards/handwired/dactyl_promicro/keymaps/pothix/keymap.c
@@ -14,10 +14,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB , KC_Q  ,KC_W   , KC_E  , KC_R  , KC_T  ,                                        KC_Y   , KC_U  , KC_I  , KC_O   , KC_P  ,KC_BSLS,
         KC_ESC,  KC_A  ,KC_S   , KC_D  , KC_F  , KC_G  ,                                        KC_H   , KC_J  , KC_K  , KC_L   ,KC_SCLN,KC_QUOT,
         KC_LSFT, KC_Z  ,KC_X   , KC_C  , KC_V  , KC_B  ,                                        KC_N   , KC_M  ,KC_COMM, KC_DOT ,KC_SLSH,KC_RSFT,
-        KC_GRV,KC_BSLS,KC_LEFT ,KC_RGHT,KC_BSPC, KC_DEL,                                        KC_SPC ,KC_ENT , KC_UP , KC_DOWN,KC_LBRC,KC_RBRC,
-                                                KC_MINS,_______,KC_RBRC,        KC_RBRC,_______,KC_MINS ,
-                                                                KC_BSLS,        KC_BSLS,
-                                                                KC_LBRC,        KC_LBRC
+        KC_GRV,KC_BSLS,KC_LEFT ,KC_RGHT,KC_LCTL, KC_SPC,                                        KC_BSPC, KC_ENT, KC_UP , KC_DOWN,KC_LBRC,KC_RBRC,
+                                                KC_LGUI,_______,TT(_LOWER),     KC_PGDN,_______,KC_LALT ,
+                                                                KC_DEL,         KC_PGUP,
+                                                                KC_LALT,        KC_ALGR
     ),
     [_LOWER] = LAYOUT_6x6(
         _______,KC_F1  ,KC_F2  ,KC_F3  ,KC_F4  ,KC_F5  ,                                       KC_F6  ,KC_F7  ,KC_F8  ,KC_F9  ,KC_F10 ,KC_F11 ,
