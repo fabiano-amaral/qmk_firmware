0d02c3406a25c3c6058ce856c829c0a1eb879b6c
Fabiano Amaral <fabiano.amaral@pagar.me>
2023-05-28 19:55:18 -0300

    config for dactyl

--- keyboards/handwired/dactyl_manuform/mine/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/mine/keymaps/default/keymap.c
@@ -9,14 +9,14 @@ extern keymap_config_t keymap_config;
 #define _____ KC_NO
 
 #define KM_A LALT_T(KC_A)
-#define KM_S LCTL_T(KC_S)
+#define KM_S LGUI_T(KC_S)
 #define KM_D LSFT_T(KC_D)
-#define KM_F LGUI_T(KC_F)
+#define KM_F LCTL_T(KC_F)
 
-#define KM_SC RALT_T(KC_SCLN)
-#define KM_L RCTL_T(KC_L)
-#define KM_K RSFT_T(KC_K)
-#define KM_J RGUI_T(KC_J)
+#define KM_SC LALT_T(KC_SCLN)
+#define KM_L LGUI_T(KC_L)
+#define KM_K LSFT_T(KC_K)
+#define KM_J LCTL_T(KC_J)
 
 // Mod-Tap
 #define SFT_BSPC LSFT_T(KC_BSPC)
@@ -34,7 +34,7 @@ extern keymap_config_t keymap_config;
 #define SFT_ENT  RSFT_T(KC_ENT)
 #define CTL_TAB  CTL_T(KC_TAB)
 #define ALT_TAB  ALT_T(KC_TAB)
-#define ALT_ESC  ALT_T(KC_ESC)
+#define ALT_ESC  LALT_T(KC_ESC)
 #define CTL_ESC  CTL_T(KC_ESC)
 
 // compose keys
@@ -42,14 +42,6 @@ extern keymap_config_t keymap_config;
 #define CMD_RTAB LCMD(KC_GRAVE)
 #define CTL_LEFT LCTL(KC_LEFT)
 #define CTL_RGHT LCTL(KC_RIGHT)
-
-// mouse keys
-#define KC_ML KC_MS_LEFT
-#define KC_MR KC_MS_RIGHT
-#define KC_MU KC_MS_UP
-#define KC_MD KC_MS_DOWN
-#define KC_MB1 KC_MS_BTN1
-#define KC_MB2 KC_MS_BTN1
 
 // active layers
 #define LY_FN  MO(_FN)
@@ -65,16 +57,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   CTL_ESC, KM_A,  KM_S,    KM_D,  KM_F,    KC_G,                         KC_H,    KM_J,  KM_K,    KM_L,   KM_SC,  KC_QUOT,\
   KC_LCMD, KC_Z,  KC_X,    KC_C,  KC_V,    KC_B,                         KC_N,    KC_M,  KC_COMM, KC_DOT, KC_SLSH,_____,  \
                   KC_LBRC, KC_RBRC,                                                      KC_MINS, KC_EQL,                 \
-                                LY_FN, CMD_SPC, SFT_DEL,     ALT_ESC, CTL_BSPC, SFT_ENT                                 \
+                                LY_FN, KC_SPC, KC_LGUI,     ALT_ESC, KC_BSPC, SFT_ENT                                 \
 ),
 
 [_FN] = LAYOUT( \
-  _____,   KC_F1,  KC_F2,  KC_F3, KC_F4, KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,     KC_F10,  KC_F11,\
-  KC_TAB,  KC_GRV,  _____,  KC_CIRC, _____, _____,                       _____,   _____,   _____,   _____,     KC_VOLU, KC_F12,\
-  _____, KC_TILD, KC_MINS, KC_PPLS, KC_PEQL, _____,                       KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT,  KC_MUTE, _____, \
-  _____, _____,  _____,  _____, _____,_____,                       KC_HOME, KC_END,  KC_PGUP, KC_PGDOWN, KC_VOLD, _____, \
-                   _____,  RESET,                                                       CTL_LEFT,  CTL_RGHT,                \
-                                 SFT_DEL,CTL_BSPC, _____,   KC_LALT, CTL_SPC, SFT_ENT                                      \
+  KC_F12, KC_F1,   KC_F2,   KC_F3,   KC_F4, KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,     KC_F10,  KC_F11,\
+  _____, KC_GRV,  _____,   KC_CIRC, _____, _____,                       KC_LBRC, KC_RBRC, _____,   KC_LCBR,   KC_RCBR, _____,\
+  _____, KC_TILD, KC_MINS, KC_PPLS, KC_PEQL, CMD_TAB,                   KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,  KC_MUTE, _____, \
+  _____, _____,   KC_UNDS, _____,   _____,_____,                        KC_HOME, KC_END,  KC_PGUP, KC_PGDOWN, KC_VOLD, _____, \
+                   _____,  RESET,                                                     CTL_LEFT,  CTL_RGHT,                \
+                                 SFT_DEL,CTL_BSPC, _____,           KC_LALT, KC_BSPC, SFT_ENT                                      \
 )};
 
 uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
